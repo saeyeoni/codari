fb66001dbf2c38778a5a2f48bebed3514bb09367
saeyeoni <sae9707@gmail.com>
2020-03-23 09:09:38 +0900

    route update

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,7 @@ Route::get('/{menu_id}', [
 
 Route::GET('/marketing/M01/items','marketing\M01Controller@getItems')->name('items');
 Route::GET('/marketing/M01/brands','marketing\M01Controller@getBrands')->name('brands');
+Route::PATCH('/marketing/M02/brands/{id}','marketing\M02Controller@delBrand');
 Route::GET('/marketing/M02/list','marketing\M02Controller@getList')->name('list');
 Route::GET('marketing/M02/{id}','marketing\M02Controller@show' );
 Route::DELETE('marketing/M02/{id}','marketing\M02Controller@softDelete' );
@@ -31,6 +32,7 @@ Route::PATCH('marketing/M02/{id}','marketing\M02Controller@update' );
 
 Route::GET('overseas/O01/items','overseas\O01Controller@getItems')->name('overitems');
 Route::GET('overseas/O01/brands','overseas\O01Controller@getBrands')->name('overbrands');
+Route::PATCH('/overseas/O02/brands/{id}','overseas\O02Controller@delBrand');
 Route::GET('overseas/O02/list','overseas\O02Controller@getList')->name('overlist');
 Route::GET('overseas/O02/{id}','overseas\O02Controller@show' );
 Route::DELETE('overseas/O02/{id}','overseas\O02Controller@softDelete' );
@@ -38,6 +40,7 @@ Route::PATCH('overseas/O02/{id}','overseas\O02Controller@update' );
 
 Route::prefix('{menu_id}')->middleware('checking')->group(function (){
     Route::GET('{pgm_id}', 'SidebarController@index');
+    Route::GET('{pgm_id}/create', 'SidebarController@create');
     Route::POST('{pgm_id}', 'SidebarController@store');
     Route::PATCH('{pgm_id}', 'SidebarController@update');
     Route::GET('{pgm_id}/{id}','SidebarController@show' );
